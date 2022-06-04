3ce5066dd8cd325a8a4669d152865c1468bd3630
Maxim <vedernikovmaxim@rambler.ru>
2022-06-04 23:34:47 +0300

    Пытался реализовать через рекурсию и обновление поля. На данном этапе во время игры не происходит * и х на поле. Возможно, проблема со скопированным списком. Вопрос - почему?

--- task_2.py
+++ task_2.py
@@ -1,16 +1,14 @@
 # "Крестики-нолики"
 
 
+import copy
 import numbers
 from random import randint
 from tabnanny import check
 from time import sleep, time
 
 
-startField = [[' ', '|', ' ', '|', ' '], ['–', '+', '–', '+', '–'], [' ', '|', ' ', '|', ' '], 
-              ['–', '+', '–', '+', '–'], [' ', '|', ' ', '|', ' ']]
-
-field = [[' ', '|', ' ', '|', ' '], ['–', '+', '–', '+', '–'],[' ', '|',' ', '|', ' '], 
+field = [[' ', '|', ' ', '|', ' '], ['–', '+', '–', '+', '–'], [' ', '|', ' ', '|', ' '],
          ['–', '+', '–', '+', '–'], [' ', '|', ' ', '|', ' ']]
 
 
@@ -20,6 +18,7 @@ def playingField(field: list):
         for j in range(len(field[i])):
             string += field[i][j]
         print(string)
+
 
 def playersMove(cellNumber, symbol='*'):
     bool = False
@@ -61,21 +60,23 @@ def playersMove(cellNumber, symbol='*'):
             bool = True
     return bool
 
+
 def computerMove():
     check = False
     while(check == False):
         cell = randint(0, 8)
         check = playersMove(cell, symbol='x')
 
+
 def winUser(text, symbol='*'):
     check = False
-    if field[0][0] == symbol and field[0][2] == symbol and field[0][3] == symbol:
+    if field[0][0] == symbol and field[0][2] == symbol and field[0][4] == symbol:
         print(text)
         check = True
-    elif field[2][0] == symbol and field[2][2] == symbol and field[2][3] == symbol:
+    elif field[2][0] == symbol and field[2][2] == symbol and field[2][4] == symbol:
         print(text)
         check = True
-    elif field[4][0] == symbol and field[4][2] == symbol and field[4][3] == symbol:
+    elif field[4][0] == symbol and field[4][2] == symbol and field[4][4] == symbol:
         print(text)
         check = True
     elif field[0][0] == symbol and field[2][0] == symbol and field[4][0] == symbol:
@@ -94,27 +95,81 @@ def winUser(text, symbol='*'):
         print(text)
         check = True
     return check
-    
+
+
 def winPc(text, symbol='x'):
-   check = winUser(text, symbol)
-   return check
+    check = winUser(text, symbol)
+    return check
 
-#пока времени не очень много, поэтому решение не очень и без какого-либо интерфейса
 
+#def game(field):
+#    print('Готов начать игру?')
+#    strUser = input()
+#    count = 9
+#    while(count > 0):
+#        print("Введите номер клетки от 1 до 9:...")
+#        playersMove(int(input()))
+#        playingField(field)
+#        check = winUser("Win!\nНачать заново?\n/break\n/exit")
+#        if check:
+#            strUser += input()
+#            break
+#        count -= 1
+#        if count == 0:
+#            print("Ходов больше не осталось!\nНачать заново?\n/break\n/exit")
+#            strUser += input()
+#        print("Ход ПК:...")
+#        sleep(2)
+#        computerMove()
+#        playingField(field)
+#        check = winPc("False!\nНачать заново?\n/break\n/exit")
+#        if check:
+#            strUser += input()
+#            break
+#        count -= 1
+#        if count == 0:
+#            print("Ходов больше не осталось!\nНачать заново?\n/break\n/exit")
+#            strUser += input()
+#    return strUser
+
+
+#def gg(field: list):
+#    fcopyUser = []
+#    fcopyUser = copy.deepcopy(field)
+#    strUser = game(fcopyUser)
+#    if strUser == '/break':
+#        gg(field)
+#    if strUser == '/exit':
+#        print('Игра окончена')
+#
+#
+#gg(field)
+
+
+print('Готов начать игру?')
+strUser = input()
 count = 9
 while(count > 0):
     print("Введите номер клетки от 1 до 9:...")
     playersMove(int(input()))
     playingField(field)
-    check = winUser("Win!")
+    check = winUser("Win!\nНачать заново?\n/break\n/exit")
     if check:
+        strUser += input()
         break
+    count -= 1
+    if count == 0:
+        print("Ходов больше не осталось!\nНачать заново?\n/break\n/exit")
+        strUser += input()
     print("Ход ПК:...")
     sleep(2)
     computerMove()
     playingField(field)
-    check = winUser("False!")
+    check = winPc("False!\nНачать заново?\n/break\n/exit")
     if check:
+        strUser += input()
         break
     count -= 1
-print("Ходов больше не осталось!")
+    if count == 0:
+        print("Ходов больше не осталось!\nНачать заново?\n/break\n/exit")
+        strUser += input()
